8da15bb080b27eee8cbb2d35f4931d7841c71c40
Katenkar23 <Katenkar.EA@mail.ru>
2020-04-07 00:06:35 +0300

    Add properties

--- Homework_3/uniq.py
+++ Homework_3/uniq.py
@@ -3,7 +3,37 @@ FILE_OUTPUT_NAME = 'src/output.txt'
 
 
 class Uniq(object):
-    # your code
+    @property
+    def count(self):
+        return self.count
+
+    @count.setter
+    def count(self, value):
+        self.count = value
+
+    @property
+    def repeat(self):
+        return self.repeat
+
+    @repeat.setter
+    def repeat(self, value):
+        self.repeat = value
+
+    @property
+    def ignore_case(self):
+        return self.ignore_case
+
+    @ignore_case.setter
+    def ignore_case(self, value):
+        self.ignore_case = value
+
+    @property
+    def unique(self):
+        return self.unique
+
+    @unique.setter
+    def unique(self, value):
+        self.unique = value
 
 if __name__ == "__main__":
     # your code
