459e56f7102bbfb09187343a7ea8c4c02ce8c173
Katenkar23 <Katenkar.EA@mail.ru>
2020-04-10 23:36:48 +0300

    Add tests

--- Homework_3/test_uniq.py
+++ Homework_3/test_uniq.py
@@ -1,4 +1,4 @@
-import pytest
+from unittest import mock
 
 from Homework_3.uniq import Uniq
 
@@ -7,20 +7,35 @@ FILE_OUTPUT_NAME = 'src/output.txt'
 
 
 def test_result_no_args():
-    assert Uniq(FILE_INPUT_NAME, FILE_OUTPUT_NAME).do_uniq() == "AB\nab\na\nA\nabc\nA3\na3\n1"
+    with mock.patch('sys.argv', ['']):
+        assert Uniq(FILE_INPUT_NAME, FILE_OUTPUT_NAME).do_uniq() == "ab\na\nabc\nAbc\n"
 
 
-# def test_content():
-#     self.fail()
-#
-#
-# def test_attributes():
-#     self.fail()
-#
-#
-# def test_attributes():
-#     self.fail()
-#
-#
-# def test_do_uniq():
-#     self.fail()
+def test_result_count():
+    my_args = ['', '-c']
+    with mock.patch('sys.argv', my_args):
+        assert Uniq(FILE_INPUT_NAME, FILE_OUTPUT_NAME).do_uniq() == "3 ab\n1 a\n2 abc\n1 Abc\n"
+
+
+def test_result_repeat():
+    my_args = ['', '-d']
+    with mock.patch('sys.argv', my_args):
+        assert Uniq(FILE_INPUT_NAME, FILE_OUTPUT_NAME).do_uniq() == "ab\nabc\n"
+
+
+def test_result_ignore_case():
+    my_args = ['', '-i']
+    with mock.patch('sys.argv', my_args):
+        assert Uniq(FILE_INPUT_NAME, FILE_OUTPUT_NAME).do_uniq() == "ab\na\nabc\n"
+
+
+def test_result_unique():
+    my_args = ['', '-u']
+    with mock.patch('sys.argv', my_args):
+        assert Uniq(FILE_INPUT_NAME, FILE_OUTPUT_NAME).do_uniq() == "a\nAbc\n"
+
+
+def test_result_repeat_unique():
+    my_args = ['', '-d', '-u']
+    with mock.patch('sys.argv', my_args):
+        assert Uniq(FILE_INPUT_NAME, FILE_OUTPUT_NAME).do_uniq() == ""
