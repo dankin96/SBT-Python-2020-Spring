ced520ada83b005d59ed7a67c51c7d26c63e1573
Katenkar23 <Katenkar.EA@mail.ru>
2020-04-10 18:25:42 +0300

    Add some code...

--- Homework_3/uniq.py
+++ Homework_3/uniq.py
@@ -1,4 +1,6 @@
-FILE_INPUT_NAME = 'src/input.txt'
+import argparse
+
+FILE_INPUT_NAME = 'src/input_1.txt'
 FILE_OUTPUT_NAME = 'src/output.txt'
 
 
@@ -7,99 +9,69 @@ class Uniq(object):
     def __init__(self, input_file, output_file, attributes):
         self.input_file = input_file
         self.output_file = output_file
-        self.take_attributes(attributes)
-
-    @property
-    def count(self):
-        return self.count
-
-    @count.setter
-    def count(self, value):
-        self.count = value
-
-    @property
-    def repeat(self):
-        return self.repeat
-
-    @repeat.setter
-    def repeat(self, value):
-        self.repeat = value
-
-    @property
-    def ignore_case(self):
-        return self.ignore_case
-
-    @ignore_case.setter
-    def ignore_case(self, value):
-        self.ignore_case = value
-
-    @property
-    def unique(self):
-        return self.unique
-
-    @unique.setter
-    def unique(self, value):
-        self.unique = value
-
-    @property
-    def input_file(self):
-        return self.input_file
-
-    @input_file.setter
-    def input_file(self, value):
-        self.input_file = value
-
-    @property
-    def output_file(self):
-        return self.output_file
-
-    @output_file.setter
-    def output_file(self, value):
-        self.output_file = value
+        self._attributes = attributes
+        self.content = input_file
 
     @property
     def content(self):
-        return self.content
+        return self._content
 
     @content.setter
     def content(self, value):
+        self._content = dict()
         with open(value) as f:
-            self.content = f.readlines()
+            for line in f:
+                line = line.replace("\n", "")
+                if line in self.content:
+                    self._content[line] += 1
+                else:
+                    self._content[line] = 1
 
     @property
     def attributes(self):
-        return self.attributes
+        return self._attributes
 
     @attributes.setter
-    def attributes(self):
-        self.attributes = 0
-
-    """def take_attribute(self, value):
-        self.count = value.count
-        self.repeat = value.repeat
-        self.ignore_case = value.ignore_case
-        self.unique = value.unique
-    """
-
-    def do_uniq_count(self):
-
-    def do_uniq_repeat(self):
-
-    def do_uniq_ignore_case(self):
-
-    def do_uniq_unique(self):
+    def attributes(self, value):
+        self._attributes = value
 
     def do_uniq(self):
-        for a in self.attributes:
-            #self.take_attribute(a)
-            if a.count:
-                self.do_uniq_count()
-            if a.repeat:
-                self.do_uniq_repeat()
-            if a.ignore_case:
-                self.do_uniq_ignore_case()
-            if a.unique:
-                self.do_uniq_unique()
+        print(self.content)
+        res = ""
+        if self.attributes.repeat and self.attributes.unique:
+            return res
+        if self.attributes.count:
+            if self.attributes.repeat:
+                for key in self.content:
+                    if self.content[key] > 1:
+                        res += str(self.content[key]) + " " + key + "\n"
+            elif self.attributes.unique:
+                for key in self.content:
+                    if self.content[key] == 1:
+                        res += str(self.content[key]) + " " + key + "\n"
+            else:
+                res = self.content
+        else:
+            if self.attributes.repeat:
+                for key in self.content:
+                    if self.content[key] > 1:
+                        res += key + "\n"
+            elif self.attributes.unique:
+                for key in self.content:
+                    if self.content[key] == 1:
+                        res += key + "\n"
+            else:
+                res = self.content.keys()
+        return res
+
 
 if __name__ == "__main__":
-    # your code
+    my_parser = argparse.ArgumentParser()
+    my_parser.add_argument("-c", "--count", help="выводить число повторов в начале каждой строки", action="store_true")
+    my_parser.add_argument("-d", "--repeat", help="выводить только повторяющиеся строки", action="store_true")
+    my_parser.add_argument("-i", "--ignore-case", help="игнорировать регистр при сравнении", action="store_true")
+    my_parser.add_argument("-u", "--unique", help="выводить только неповторяющиеся строки", action="store_true")
+    args = my_parser.parse_args()
+    result = Uniq(FILE_INPUT_NAME, FILE_OUTPUT_NAME, args).do_uniq()
+    print("Result: ")
+    print(result)
