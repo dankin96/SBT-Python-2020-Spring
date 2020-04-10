74184247a73ab8a577727c5aaf0aebfd7753ebf4
Katenkar23 <Katenkar.EA@mail.ru>
2020-04-10 19:09:24 +0300

    Add tests

---
+++ Homework_3/test_uniq.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestUniq(TestCase):
+    def test_content(self):
+        self.fail()
+
+    def test_content(self):
+        self.fail()
+
+    def test_attributes(self):
+        self.fail()
+
+    def test_attributes(self):
+        self.fail()
+
+    def test_do_uniq(self):
+        self.fail()
