cbae58796d1c16012d7357b7e3235ecf340002ed
Maria <ermolaeva.m.vla@sberbank.ru>
2020-04-14 16:29:26 +0300

    first commit

--- Homework_3/uniq.py
+++ Homework_3/uniq.py
@@ -1,9 +1,79 @@
-FILE_INPUT_NAME = 'src/input.txt'
-FILE_OUTPUT_NAME = 'src/output.txt'
+import argparse
+
+FILE_INPUT_NAME = 'src/input_3.txt'
+FILE_OUTPUT_NAME = 'src/output_3_my.txt'
 
 
 class Uniq(object):
-    # your code
+    strings_dict = {}
+    input_file = ""
+    output_file = ""
+
+    def __init__(self, input_file, output_file):
+        self.strings_dict = {}
+        self.input_file = input_file
+        self.output_file = output_file
+
+    @staticmethod
+    def configure_argparser():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-c", "--count", action='store_true',
+                            help="print the number of repetitions at the beginning of each line")
+        parser.add_argument("-d", "--repeat", action='store_true', help="print only repeated lines")
+        parser.add_argument("-i", "--ignore_case", action='store_true', help="ignore case during comparing")
+        parser.add_argument("-u", "--unique", action='store_true', help="print only non-repeating lines")
+        return parser.parse_args()
+
+    def read(self, ignore_case, filename):
+        file = open(filename)
+        for line in file.readlines():
+            string = line.strip()
+
+            if ignore_case:
+                string = string.lower()
+
+            if string not in self.strings_dict:
+                self.strings_dict[string] = 0
+
+            self.strings_dict[string] = self.strings_dict.get(string) + 1
+            file.close()
+
+    def repeat(self):
+        for string in list(self.strings_dict):
+            if self.strings_dict[string] == 1:
+                del self.strings_dict[string]
+
+    def unique(self):
+        for string in list(self.strings_dict):
+            if self.strings_dict[string] != 1:
+                del self.strings_dict[string]
+
+    def write(self, count, filename):
+        file = open(filename, 'w')
+        if count:
+            for string in self.strings_dict:
+                file.write(str(self.strings_dict[string]) + " " + string + '\n')
+        else:
+            for string in self.strings_dict:
+                file.write(string + '\n')
+        file.close()
+
+    def main(self):
+        ignore_case = False
+        count = False
+        args = Uniq.configure_argparser()
+        if args.ignore_case:
+            ignore_case = True
+        if args.count:
+            count = True
+        self.read(ignore_case, self.input_file)
+        if args.repeat:
+            self.repeat()
+        if args.unique:
+            self.unique()
+        self.write(count, self.output_file)
+
 
 if __name__ == "__main__":
-    # your code
+    uniq = Uniq(FILE_INPUT_NAME, FILE_OUTPUT_NAME)
+    uniq.main()
---
+++ Homework_3/uniq_tests.py
@@ -0,0 +1,45 @@
+import unittest
+
+from Homework_3.uniq import Uniq
+
+FILE_INPUT_NAME = 'src/input_3.txt'
+FILE_OUTPUT_NAME = 'src/output_3_my.txt'
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.uniq = Uniq(FILE_INPUT_NAME, FILE_OUTPUT_NAME)
+
+    def test_repeat_not_ignore_case(self):
+        self.uniq.read(False, self.uniq.input_file)
+        self.uniq.repeat()
+        self.assertEqual({'ab': 3, 'abc': 2}, self.uniq.strings_dict)
+
+    def test_repeat_ignore_case(self):
+        self.uniq.read(True, self.uniq.input_file)
+        self.uniq.repeat()
+        self.assertEqual({'ab': 4, 'a': 2, 'abc': 2, 'a3': 2}, self.uniq.strings_dict)
+
+    def test_unique_not_ignore_case(self):
+        self.uniq.read(False, self.uniq.input_file)
+        self.uniq.unique()
+        self.assertEqual({'AB': 1, 'a': 1, 'A': 1, 'A3': 1, 'a3': 1, '1': 1}, self.uniq.strings_dict)
+
+    def test_unique_ignore_case(self):
+        self.uniq.read(True, self.uniq.input_file)
+        self.uniq.unique()
+        self.assertEqual({'1': 1}, self.uniq.strings_dict)
+
+    def test_output_file_count_ignore_case(self):
+        strings = []
+        self.uniq.read(True, self.uniq.input_file)
+        self.uniq.write(True, FILE_OUTPUT_NAME)
+        file = open(FILE_OUTPUT_NAME)
+        for line in file.readlines():
+            line = line.strip()
+            strings.append(line)
+        self.assertEqual('4 ab', strings[0])
+        file.close()
+
+
+if __name__ == '__main__':
+    unittest.main()
